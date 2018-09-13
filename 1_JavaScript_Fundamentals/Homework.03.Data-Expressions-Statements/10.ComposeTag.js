ca8b97714b59cf6d582ea36afaac3680a1728791
Hristo-R <hristo.razpopov@abv.bg>
2018-09-13 21:47:23 +0300

    added Homework.03.Data-Expressions-Statements/10.ComposeTag.js

---
+++ 1_JavaScript_Fundamentals/Homework.03.Data-Expressions-Statements/10.ComposeTag.js
@@ -0,0 +1,7 @@
+function composeTag(input) {
+    let fileLocation = input[0];
+    let alternateText = input[1];
+    console.log(`<img src="${fileLocation}" alt="${alternateText}">`)
+}
+
+composeTag(['smiley.gif', 'Smiley Face']);
