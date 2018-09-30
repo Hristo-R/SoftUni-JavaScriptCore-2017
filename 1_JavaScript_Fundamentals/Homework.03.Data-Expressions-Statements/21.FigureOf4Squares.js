b63deb0f4220f5904f9d2da09e5b617d19c944a9
Hristo-R <hristo.razpopov@abv.bg>
2018-09-30 13:35:27 +0300

    added Homework.03.Data-Expressions-Statements/21.FigureOf4Squares.js

---
+++ 1_JavaScript_Fundamentals/Homework.03.Data-Expressions-Statements/21.FigureOf4Squares.js
@@ -0,0 +1,14 @@
+function printFigure(num) {
+    let number = Number(num);
+    let length = number % 2 == 0 ? number - 1 : number;
+
+    for(let i = 1; i<= length; i++) {
+        if(i == 1 || i == length || (length +1)/i == 2) {
+            console.log("+" + "-".repeat(number-2) + "+" + "-".repeat(number-2) + "+");
+        } else {
+            console.log("|" + " ".repeat(number-2) + "|" + " ".repeat(number-2) + "|");
+        }
+    }
+}
+
+printFigure(10);
