9c6d9a3af64a0df642de326802a7d546016e9b7c
Hristo-R <hristo.razpopov@abv.bg>
2019-05-30 13:13:57 +0300

    Added 04.SumByTown.js

---
+++ 1_JavaScript_Fundamentals/Homework.10.Objects-and-Assoc-Arrays/04.SumByTown.js
@@ -0,0 +1,25 @@
+function sumByTown(input) {
+    let towns = {};
+    for (let i = 0; i < input.length; i+=2) {
+        let town = input[i];
+        let value = Number(input[i+1]);
+        if (!towns.hasOwnProperty(town)) {
+            towns[town] = 0;
+        }
+        towns[town] += value;
+    }
+
+    let json = JSON.stringify(towns);
+    console.log(json);
+}
+
+sumByTown([
+    'Sofia',
+    '20',
+    'Varna',
+    '3',
+    'Sofia',
+    '5',
+    'Varna',
+    '4'
+]);
