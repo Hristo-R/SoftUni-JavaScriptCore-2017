4b4dad0e1cd78993d881bce4b025c3606b005fcb
Hristo-R <hristo.razpopov@abv.bg>
2019-09-08 09:46:09 +0300

    Edited link-buttons.js

--- 2_JavaScript_Advanced/Homework.05.jQuery-Library/link-buttons.js
+++ 2_JavaScript_Advanced/Homework.05.jQuery-Library/link-buttons.js
@@ -1,5 +1,6 @@
 function attachEvents() {
   $('a.button').on('click', buttonClicked);
+  
   function buttonClicked() {
     $('.selected').removeClass('selected');
     $(this).addClass('selected');
