32ad4b21ae19e330668137654cf93a58b01a1bab
Hristo-R <hristo.razpopov@abv.bg>
2019-10-13 23:51:02 +0300

    Added 02.Fibonacci.js

---
+++ 2_JavaScript_Advanced/Homework.09.Object-Composition-LAB/02.Fibonacci.js
@@ -0,0 +1,25 @@
+function solve() {
+    function getFibonator() {
+        let f0 = 0, f1 = 1;
+        return function() {
+            let f2 = f0 + f1;
+            f0 = f1;
+            f1 = f2;
+            return f1;
+        };
+    }
+
+    return fib();
+}
+
+function getFibonator() {
+    let f0 = 0, f1 = 1;
+    return function() {
+        let f2 = f0 + f1;
+        f0 = f1;
+        f1 = f2;
+        return f1;
+    };
+}
+
+
