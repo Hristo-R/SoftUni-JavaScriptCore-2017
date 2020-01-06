a9c716bebd938ba20ebed51c25591d72ca854d19
Hristo-R <hristo.razpopov@abv.bg>
2020-01-07 00:57:49 +0200

    Added Homework.13.Classes-and-Class-Members-LAB/1.Rectangle.js

---
+++ 2_JavaScript_Advanced/Homework.13.Classes-and-Class-Members-LAB/1.Rectangle.js
@@ -0,0 +1,21 @@
+class Rectangle {
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+
+let rect = new Rectangle(5, 4, 'blue');
+console.log(rect);
+
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+
+let area = rect.calcArea();
+console.log(area);
