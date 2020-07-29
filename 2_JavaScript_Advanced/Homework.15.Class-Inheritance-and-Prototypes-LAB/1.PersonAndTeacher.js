4999872484f82ad23d8ff19a91693bf44b5a3c88
Hristo R <hristo.razpopov@abv.bg>
2020-07-30 00:00:12 +0300

    Update 1.PersonAndTeacher.js

--- 2_JavaScript_Advanced/Homework.15.Class-Inheritance-and-Prototypes-LAB/1.PersonAndTeacher.js
+++ 2_JavaScript_Advanced/Homework.15.Class-Inheritance-and-Prototypes-LAB/1.PersonAndTeacher.js
@@ -16,7 +16,7 @@ function personAndTeacherClasses() {
     return {Person, Teacher};
 }
 
-//let person = new Person('Ivo', 'ivo@ivo.bg');
+let person = new Person('Ivo', 'ivo@ivo.bg');
 //console.log(person);
 //
 //let teacher = new Teacher('Ivelin', 'ivo@ivo.bg', 'swimming');
